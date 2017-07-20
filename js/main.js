e9a3dde46b093e8b27a9f709edab601c0f9691cf
Jacs19 <jacspro@gmail.com>
2017-07-20 17:06:48 -0400

    Se agregan datos dummy y se finaliza paginacion de tabla

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
     'use strict';
 
     angular.module('main', ['ui.bootstrap'])
-		.controller('AppController', AppController);		
+		.controller('AppController', AppController)
+		.filter('capitalizar', Capitalizar);
 		
 	//---------->[INYECCION DEPENDENCIAS]<--------------------------------------
     AppController.$inject = ['$scope', '$uibModal', '$filter'];
@@ -13,13 +14,181 @@
         //Scope
         var vm = this;
 		
-		//Variables
-		vm.inicio = "Hola !!!";
+		//Variables									
+		vm.lista = 
+		[
+			{
+				id			: "1",
+				nombre		: "jhona",
+				fechaInicio	: "20/07/2017",
+				fechaFin	: "22/07/2017",
+				estado		: "pendiente",
+				monto		: "2300"
+			},
+			{
+				id			: "2",
+				nombre		: "jhona",
+				fechaInicio	: "20/07/2017",
+				fechaFin	: "22/07/2017",
+				estado		: "pendiente",
+				monto		: "2300"
+			},
+			{
+				id			: "3",
+				nombre		: "jhona",
+				fechaInicio	: "20/07/2017",
+				fechaFin	: "22/07/2017",
+				estado		: "pendiente",
+				monto		: "2300"
+			},
+			{
+				id			: "4",
+				nombre		: "jhona",
+				fechaInicio	: "20/07/2017",
+				fechaFin	: "22/07/2017",
+				estado		: "pendiente",
+				monto		: "2300"
+			},
+			{
+				id			: "5",
+				nombre		: "jhona",
+				fechaInicio	: "20/07/2017",
+				fechaFin	: "22/07/2017",
+				estado		: "pendiente",
+				monto		: "2300"
+			},{
+				id			: "6",
+				nombre		: "jhona",
+				fechaInicio	: "20/07/2017",
+				fechaFin	: "22/07/2017",
+				estado		: "pendiente",
+				monto		: "2300"
+			},
+			{
+				id			: "7",
+				nombre		: "jhona",
+				fechaInicio	: "20/07/2017",
+				fechaFin	: "22/07/2017",
+				estado		: "pendiente",
+				monto		: "2300"
+			},{
+				id			: "8",
+				nombre		: "jhona",
+				fechaInicio	: "20/07/2017",
+				fechaFin	: "22/07/2017",
+				estado		: "pendiente",
+				monto		: "2300"
+			},
+			{
+				id			: "9",
+				nombre		: "jhona",
+				fechaInicio	: "20/07/2017",
+				fechaFin	: "22/07/2017",
+				estado		: "pendiente",
+				monto		: "2300"
+			},
+			{
+				id			: "10",
+				nombre		: "jhona",
+				fechaInicio	: "20/07/2017",
+				fechaFin	: "22/07/2017",
+				estado		: "pendiente",
+				monto		: "2300"
+			},
+			{
+				id			: "11",
+				nombre		: "mario",
+				fechaInicio	: "21/07/2017",
+				fechaFin	: "23/07/2017",
+				estado		: "procesada",
+				monto		: "10000"
+			},
+			{
+				id			: "12",
+				nombre		: "mario",
+				fechaInicio	: "21/07/2017",
+				fechaFin	: "23/07/2017",
+				estado		: "procesada",
+				monto		: "10000"
+			},
+			{
+				id			: "13",
+				nombre		: "mario",
+				fechaInicio	: "21/07/2017",
+				fechaFin	: "23/07/2017",
+				estado		: "procesada",
+				monto		: "10000"
+			},
+			{
+				id			: "14",
+				nombre		: "mario",
+				fechaInicio	: "21/07/2017",
+				fechaFin	: "23/07/2017",
+				estado		: "procesada",
+				monto		: "10000"
+			},
+			{
+				id			: "15",
+				nombre		: "mario",
+				fechaInicio	: "21/07/2017",
+				fechaFin	: "23/07/2017",
+				estado		: "procesada",
+				monto		: "10000"
+			},
+			{
+				id			: "16",
+				nombre		: "mario",
+				fechaInicio	: "21/07/2017",
+				fechaFin	: "23/07/2017",
+				estado		: "procesada",
+				monto		: "10000"
+			},
+			{
+				id			: "17",
+				nombre		: "mario",
+				fechaInicio	: "21/07/2017",
+				fechaFin	: "23/07/2017",
+				estado		: "procesada",
+				monto		: "10000"
+			},
+			{
+				id			: "18",
+				nombre		: "mario",
+				fechaInicio	: "21/07/2017",
+				fechaFin	: "23/07/2017",
+				estado		: "procesada",
+				monto		: "10000"
+			}		
+		];
 		
-		vm.totalItems = 40;
-		vm.currentPage = 4;					
+		vm.totalItems 	= vm.lista.length;
+		vm.pageSize 	= 5;
+		vm.currentPage 	= 1;	
+		vm.arrAux 		= vm.lista.slice(((vm.currentPage * vm.pageSize) - vm.pageSize), (vm.currentPage * vm.pageSize));
+		
+		vm.pageChange = pageChange;
+		
+		function pageChange(){
+			vm.arrAux = vm.lista.slice(((vm.currentPage * vm.pageSize) - vm.pageSize), (vm.currentPage * vm.pageSize));
+		}
 		
 	}
 	//---------->[FIN CONTROLADOR PRINCIPAL]<-----------------------------------
+	
+	function Capitalizar() {
+        return function (texto) {
+            if (texto === undefined)
+                return '';
+
+            var palabras = texto.toLowerCase().split(' ');
+
+            if (palabras.length > 0) {
+                for (var i = 0; i < palabras.length; i++) {
+                    palabras[i] = palabras[i].trim().charAt(0).toUpperCase() + palabras[i].substr(1);
+                }
+            }
+            return palabras.join(' ');
+        };
+    }
 		
 })();
