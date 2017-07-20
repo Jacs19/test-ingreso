3323e67985e7b4ef92b5f948d652d7289c1c00b4
Jacs19 <jacspro@gmail.com>
2017-07-20 10:29:28 -0400

    Maqueta terminada, falta funcionalidad - adelanto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+(function () {
+    'use strict';
 
+    angular.module('main', ['ui.bootstrap'])
+		.controller('AppController', AppController);		
+		
+	//---------->[INYECCION DEPENDENCIAS]<--------------------------------------
+    AppController.$inject = ['$scope', '$uibModal', '$filter'];
+	
+    //---------->[CONTROLADOR PRINCIPAL]<---------------------------------------
+    function AppController($scope, $uibModal, $filter) {
+		//------------------------->[DECLARACION VARIABLES]<--------------------
+        //Scope
+        var vm = this;
+		
+		//Variables
+		vm.inicio = "Hola !!!";
+		
+		vm.totalItems = 40;
+		vm.currentPage = 4;					
+		
+	}
+	//---------->[FIN CONTROLADOR PRINCIPAL]<-----------------------------------
+		
+})();
